654e7b1ef69b56203855011a061076479abdd6fa
Billy Barker <billy.barker@QUALITEST.LOCAL>
2021-09-29 10:43:01 +0100

    Add look at wishlist and whole checker

--- src/test/java/stepDefintions/AddToWishlist.java
+++ src/test/java/stepDefintions/AddToWishlist.java
@@ -1,13 +1,19 @@
 package stepDefintions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
 
 public class AddToWishlist {
     public WebDriver driver;
 
-    @Given("I add four different products to my wish list\n")
+    @Given("I add four different products to my wish list")
     public void user_add_items_to_wishlist(String url){
         new AddItemsToWishlist();
+    }
+
+    @When("I view my wishlist table")
+    public void user_views_wishlist(){
+        new LookAtWishList();
     }
 }
